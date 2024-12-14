ddf382611ed27e890928bf35f0c97ff56086d723
Ajay Chavan <ajaychavancse@gmail.com>
2024-12-14 17:22:23 +0530

    Update recruiter.route.js

--- backend/routes/recruiter.route.js
+++ backend/routes/recruiter.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, register, updateProfile, getImage } from "../controllers/recruiter.controller.js";
+import { login, logout, register, updateProfile, getImage, getGst } from "../controllers/recruiter.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { singleUpload } from "../middlewares/mutler.js";
  
@@ -10,6 +10,7 @@ router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
 router.route("/image/:bId").get(getImage);
+router.route("/gst/:no").get(getGst);
 
 export default router;
 
