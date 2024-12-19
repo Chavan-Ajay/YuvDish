26220533e03304b8c99bad19094e47d6c034a19b
Ajay Chavan <ajaychavancse@gmail.com>
2024-12-19 23:34:39 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,10 +1,11 @@
 const serverUrlOnline = "https://yuvdish.onrender.com"
 const serverUrlOnline2 = "https://yuv-dish.vercel.app"
 const serverUrlOnline3 = "https://yuvdish-6uf3.onrender.com"
+const serverUrlOnline4 = "https://yuvdish-nrur.onrender.com"
 
 
 const serverUrlOffline = "http://localhost:3000"
-const serverUrl = serverUrlOnline3
+const serverUrl = serverUrlOnline4
 export const USER_API_END_POINT=`${serverUrl}/api/v1/user`;
 export const JOB_API_END_POINT=`${serverUrl}/api/v1/job`;
 export const APPLICATION_API_END_POINT=`${serverUrl}/api/v1/application`;
